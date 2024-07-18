95b58a890e646a9ffd30bdec2c95775dc453f9d1
Aron Ciruela <ciruela.aron@gmail.com>
2024-07-18 15:49:35 +0800

    Initial commit

---
+++ src/main/java/com/ciruela/usermanagement/UserManagermentApplication.java
@@ -0,0 +1,13 @@
+package com.ciruela.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagermentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagermentApplication.class, args);
+	}
+
+}
